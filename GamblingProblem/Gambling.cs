257aae8e879bdae6c8b31c4cd0b3a9cda7746062
ka1peshc <kalpesh111997@gmail.com>
2021-10-06 09:56:56 +0530

    Define constant for bet and stake

---
+++ GamblingProblem/Gambling.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblingProblem
+{
+    class Gambling
+    {
+        public const int STAKE = 100;
+        public const int EVERYGAMEBET = 1;
+    }
+}

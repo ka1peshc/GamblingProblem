4ece4d81de921740e3a1856f91d56f8326a35b8a
ka1peshc <kalpesh111997@gmail.com>
2021-10-06 10:50:35 +0530

    Add: StartGambling function to get if gambler loose or win

--- GamblingProblem/Gambling.cs
+++ GamblingProblem/Gambling.cs
@@ -8,5 +8,21 @@ namespace GamblingProblem
     {
         public const int STAKE = 100;
         public const int EVERYGAMEBET = 1;
+
+        public void StartGambling()
+        {
+            int wallet = STAKE;
+            Random random = new Random();
+            int result = random.Next(0, 2);
+            if(result == 0)
+            {
+                wallet--;
+            }
+            else
+            {
+                wallet++;
+            }
+            Console.WriteLine(wallet);
+       }
     }
 }
--- GamblingProblem/Program.cs
+++ GamblingProblem/Program.cs
@@ -4,9 +4,11 @@ namespace GamblingProblem
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
+            Gambling gm = new Gambling();
+            gm.StartGambling();
         }
     }
 }

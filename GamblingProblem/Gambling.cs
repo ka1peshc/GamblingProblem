6aca1cec55fb56a46b74feb403295956fb8dcc70
ka1peshc <kalpesh111997@gmail.com>
2021-10-06 11:58:32 +0530

    Add: Logic->Play till your stake is 50% lost or 50% is gain

--- GamblingProblem/Gambling.cs
+++ GamblingProblem/Gambling.cs
@@ -8,21 +8,34 @@ namespace GamblingProblem
     {
         public const int STAKE = 100;
         public const int EVERYGAMEBET = 1;
+        public const int STOPWINSTAKE = 150;
+        public const int STOPLOOSESTAKE = 50;
 
         public void StartGambling()
         {
             int wallet = STAKE;
+            int result;
+            int BetCount = 0;
             Random random = new Random();
-            int result = random.Next(0, 2);
-            if(result == 0)
+            while (true)
             {
-                wallet--;
+                result = random.Next(0, 2);
+                if (result == 0)
+                {
+                    wallet--;
+                }
+                else
+                {
+                    wallet++;
+                }
+                if(wallet > STOPWINSTAKE || wallet < STOPLOOSESTAKE)
+                {
+                    
+                    break;
+                }
+                BetCount++;
             }
-            else
-            {
-                wallet++;
-            }
-            Console.WriteLine(wallet);
+            Console.WriteLine("Stake is {0} Number of times bet placed {1}", wallet,BetCount);
        }
     }
 }
